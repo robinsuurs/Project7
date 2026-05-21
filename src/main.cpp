c509f035c56699b8d7ddfccbc5c16acd496d7e04
Robin Suurs <robin.suurs@gmail.com>
2026-05-21 16:56:22 +0200

    Calibration added
    seems like a contrast issue for the test line detection

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,7 @@ constexpr uint8_t QTRPins[SensorCount] = {A0, A1, A2, A3, A4, A5, A6};
 
 
 void setup() {
-  Wire.begin();
-  Wire.setClock(400000); //400khz clock
+
   Serial.begin(9600);
   while (!Serial) {
     ;
@@ -18,11 +17,43 @@ void setup() {
   qtr.setTypeAnalog();
   qtr.setSensorPins(QTRPins, SensorCount);
 
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, HIGH); // turn on Arduino's LED to indicate we are in calibration mode
+
+  // analogRead() takes about 0.1 ms on an AVR.
+  // 0.1 ms per sensor * 4 samples per sensor read (default) * 7 sensors
+  // * 10 reads per calibrate() call = ~28 ms per calibrate() call.
+  // Call calibrate() 200 times to make calibration take about 5 seconds.
+  for (uint8_t i = 0; i < 200; i++)
+  {
+    qtr.calibrate();
+  }
+  digitalWrite(LED_BUILTIN, LOW); // turn off Arduino's LED to indicate we are through with calibration
+
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.minimum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+
+  // print the calibration maximum values measured when emitters were on
+  for (uint8_t i = 0; i < SensorCount; i++)
+  {
+    Serial.print(qtr.calibrationOn.maximum[i]);
+    Serial.print(' ');
+  }
+  Serial.println();
+  Serial.println();
+  delay(1000);
 
 }
 
 void loop() {
-  qtr.read(sensorValues, QTRReadMode::Manual);
+
+  // read calibrated sensor values and obtain a measure of the line position
+  // from 0 to 7000
+  qtr.readLineBlack(sensorValues, QTRReadMode::On);
 
   for (uint8_t i = 0; i < SensorCount; i++)
   {
